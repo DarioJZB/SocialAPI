7e678e5960449168c65a78716743f06caca95b39
DarioJZB <dariojzb87@gmail.com>
2024-11-07 18:12:30 -0500

    all routes set up

---
+++ src/controllers/userController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express"
+
+export const getAllUsers = async () => {
+
+}
+
+export const getUserById = async (req: Request, res: Response) => {
+
+}
+
+export const createUser = async (req: Request, res: Response) => {
+
+}
+
+export const updateUser = async () => {
+
+}
+
+export const deleteUser = async () => {
+
+}
+
+export const addFriend = async () => {
+
+}
+
+export const deleteFriend = async () => {
+    
+}
--- src/routes/api/userRoutes.ts
+++ src/routes/api/userRoutes.ts
@@ -1,9 +1,27 @@
 import { Router } from 'express';
+import { 
+    addFriend,
+    createUser, 
+    deleteFriend, 
+    deleteUser, 
+    getAllUsers, 
+    getUserById, 
+    updateUser 
+} from '../../controllers/userController';
 
 const router = Router();
 
 // /api/users
 router.route('/').get(getAllUsers);
+router.route('/').post(createUser);
 
-// /api/:userId
-router.route('/:userId').get(setUserById);
+// /api/users/:userId
+router.route('/:userId').get(getUserById);
+router.route('/:userId').put(updateUser);
+router.route('/:userId').delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route(':userId/friends/:friendId').post(addFriend);
+router.route(':userId/friends/:friendId').delete(deleteFriend);
+
+export { router as userRouter };
